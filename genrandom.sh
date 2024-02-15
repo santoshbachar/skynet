991ec29dfbe38a518778ac156efbe260db6a4d63
Santosh Bachar <santoshbachar@gmail.com>
2024-02-15 22:27:38 +0530

    Refactor to add range

--- genrandom.sh
+++ genrandom.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-R=`shuf -i $1 $2 -n 1`
-echo $R
-sleep $R
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <min> <max>"
+    exit 1
+fi
+
+min="$1"
+max="$2"
+
+random_number=$(shuf -i "$min"-"$max" -n 1)
+
+echo $random_number
+sleep $random_number
